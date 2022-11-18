6e30ee11304aad525a330728a9ba84a2ecad873a
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-17 21:28:58 -0300

    feat(backend): Cria o método cashReceived no account service

--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -27,6 +27,13 @@ export class AccountsService {
     return await this.accountRepository.save(accountSend);
   }
 
+  async cashReceived(id: string, value: number) {
+    const accountReceive = await this.findAccountById(id);
+    accountReceive.balance = Number(accountReceive.balance) + value;
+    accountReceive.balance = Number(accountReceive.balance.toFixed(2));
+    return await this.accountRepository.save(accountReceive);
+  }
+
   async findAccountById(id: string) {
     const account = await this.accountRepository.findOneBy({ id });
     return account;
