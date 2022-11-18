e3e1b2511d902fd2d388c3f370a5f5ada6e59069
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-17 22:34:25 -0300

    feat(backend): Adiciona referencia entre module user e transactions

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsModule } from 'src/accounts/accounts.module';
 import { Transaction } from 'src/entities';
+import { UsersModule } from 'src/users/users.module';
+import { TransactionsService } from './transactions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction]),
+    forwardRef(() => UsersModule),
+    AccountsModule,
+  ],
+  providers: [TransactionsService],
+  exports: [TransactionsService],
 })
 export class TransactionsModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities';
+import { TransactionsModule } from 'src/transactions/transactions.module';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => TransactionsModule),
+  ],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
