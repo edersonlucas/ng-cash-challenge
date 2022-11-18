9b65ebef68d7eccda54cc9b890386aba3016cef3
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-17 21:18:46 -0300

    feat(backend): Cria o module accounts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { OrmConfig } from './database/ormConfig';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(OrmConfig as TypeOrmModuleOptions)],
+  imports: [TypeOrmModule.forRoot(OrmConfig as TypeOrmModuleOptions), AccountsModule],
   controllers: [],
   providers: [],
 })
