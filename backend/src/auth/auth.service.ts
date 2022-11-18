12bc38e70b4a837feaba538ddb7a9391fb96cf44
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-17 22:45:21 -0300

    feat(backend): Cria o método register no auth service

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { AccountsService } from 'src/accounts/accounts.service';
+import { MessagesHelper } from '../helpers';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly accountService: AccountsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    await this.userAlreadyExists(createUserDto.username);
+    const newAccount = await this.accountService.create();
+    const newUser = {
+      ...createUserDto,
+      account: newAccount,
+    };
+    const { account, username } = await this.userService.create(newUser);
+    const payload = {
+      sub: account.id,
+      username,
+    };
+    const token = this.jwtService.sign(payload);
+    return { username, balance: account.balance, token };
+  }
+
+  private async userAlreadyExists(username: string) {
+    const user = await this.userService.findByUsername(username);
+    if (user)
+      throw new HttpException(
+        MessagesHelper.USER_ALREADY_EXISTS,
+        HttpStatus.CONFLICT,
+      );
+  }
+}
