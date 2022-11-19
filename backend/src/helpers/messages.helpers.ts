cded886f0d98fd6af939154aea8ca1333381f7bc
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-19 16:22:44 -0300

    refactor(backend): Muda o enum MessageHelper para um objeto javascript

--- backend/src/helpers/messages.helpers.ts
+++ backend/src/helpers/messages.helpers.ts
@@ -1,9 +1,11 @@
-export enum MessagesHelper {
-  PASSWORD_INVALID = 'The password must contain at least 1 capital letter, 1 number.',
-  USERNAME_OR_PASSWORD_INVALID = 'Invalid username and/or password.',
-  USER_ALREADY_EXISTS = 'User already exists.',
-  INSUFFICIENT_BALANCE = 'The account does not have sufficient balance for this transaction.',
-  INVALID_TRANSFER = 'This transfer is invalid.',
-  USER_NOT_FOUND = 'User not found.',
-  TOKEN_INVALID = 'Your token is invalid.',
-}
+export const MessagesHelper = {
+  PASSWORD_INVALID:
+    'The password must contain at least 1 capital letter, 1 number.',
+  USERNAME_OR_PASSWORD_INVALID: 'Invalid username and/or password.',
+  USER_ALREADY_EXISTS: 'User already exists.',
+  INSUFFICIENT_BALANCE:
+    'The account does not have sufficient balance for this transaction.',
+  INVALID_TRANSFER: 'This transfer is invalid.',
+  USER_NOT_FOUND: 'User not found.',
+  TOKEN_INVALID: 'Your token is invalid.',
+};
