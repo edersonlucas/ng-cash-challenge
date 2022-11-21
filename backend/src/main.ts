3614dff89cebc1cb83883281ac66851ced1d6e82
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-21 15:38:27 -0300

    chore(backend): Configura o CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.setGlobalPrefix('/api/v1');
   await app.listen(Number(process.env.PORT_BACKEND) || 3001);
 }
