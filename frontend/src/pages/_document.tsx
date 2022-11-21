e0dc5d2da1b87be8b9bcb326a69273db88b7c696
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-21 16:16:06 -0300

    chore(frontend): Adiciona favicon e a fonte padrão da NG.CASH

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
