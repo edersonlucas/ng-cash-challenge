afcc7de065c778a6afc27e02e042be79b1a6e9bc
Ederson Lucas <edersonlucas@outlook.com.br>
2022-11-21 16:06:56 -0300

    chore(frontend): Configura o AuthProvider e o Header no _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import Header from '../components/Header';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex flex-col min-h-screen bg-bottom bg-none bg-no-repeat bg- lg:bg-black-900 mx-auto">
-      <Component {...pageProps} />
-    </div>
-  )
-  }
+    <AuthProvider>
+      <div className="flex flex-col min-h-screen bg-bottom bg-none bg-no-repeat bg- bg-black-900 text-white-900 mx-auto">
+        <Header />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </div>
+      <ToastContainer />
+    </AuthProvider>
+  );
+}
